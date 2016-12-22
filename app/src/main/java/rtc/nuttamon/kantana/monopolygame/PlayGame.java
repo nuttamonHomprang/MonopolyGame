b16c3c312d09a1fd35b03a03c8f497a0f4556d68
masterUNG <phrombutr@gmail.com>
2016-12-22 23:07:45 +0700

    โจทย์ ข้อแรกได้ละ

--- app/src/main/java/rtc/nuttamon/kantana/monopolygame/PlayGame.java
+++ app/src/main/java/rtc/nuttamon/kantana/monopolygame/PlayGame.java
@@ -14,6 +14,7 @@ public class PlayGame extends AppCompatActivity {
     private TextView questionTextView;
     private RadioButton[] choiceRadioButtons;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
